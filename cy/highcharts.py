67ebd0774053500af535c04ced1b6aec88008e56
lynn0823 <313857119@qq.com>
2017-06-16 18:04:08 +0800

    highcharts 测试

---
+++ cy/highcharts.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,HttpResponseRedirect,redirect,HttpResponse
+from models import  *
+from forms import *
+from django.http import JsonResponse
+from django.db.models import Sum
+
+def area_arr(req):
+    area_a=datadiff.objects.raw('''
+            SELECT
+                    areaname,
+                    sum(amou) shopamount
+                    
+            FROM
+                (
+                    SELECT
+                        datadiff.sys_amount AS amou,
+                        `shop-info`.sName AS n,
+                        area.`name` AS areaname,
+                        managers.`name` as man,
+                        MONTH(datadiff.date) as m
+                 
+                    FROM
+                        datadiff
+                    
+                    LEFT JOIN `shop-info` ON datadiff.id_shop = `shop-info`.Id
+                    LEFT JOIN area ON `shop-info`.areaId = area.Id
+                    LEFT JOIN managers on `shop-info`.managerId=managers.id
+                   WHERE MONTH (datadiff.date)=5
+                ) AS basicquery
+            GROUP BY areaname
+    ''').query
+    data=[]
+    categories=[]
+    # categories=tuple(area_a.cursor[0])
+    for i in tuple(area_a):
+        data.append(i[0])
+        categories.append(i[1])
+
+    return  render(req,'highcharts/highchart.html',locals())
